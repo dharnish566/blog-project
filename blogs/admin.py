713622ba223acc51ec665b65e89bee74944dac15
dharnish566 <pdharnishdharnish@gmail.com>
2026-09-24 21:54:50 +0530

    create blog app & model

---
+++ blogs/admin.py
@@ -0,0 +1,15 @@
+from django.contrib import admin 
+from .models import Category , Blog
+
+# Register your models here.
+
+class BlogAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug' : ('title',)}
+    list_display = ('title' , 'author' , 'category' , 'is_featured' , 'status')
+    search_fields = ('title' , 'author' , 'id' , 'category__category_name' , 'status')
+    
+
+admin.site.register(Category)
+admin.site.register(Blog , BlogAdmin)
+
+
